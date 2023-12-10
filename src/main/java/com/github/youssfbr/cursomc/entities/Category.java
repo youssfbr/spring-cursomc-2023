3c3880b6421d2ac3e7640c06fd5e256f124c325b
Alisson Youssf <youssfbr@gmail.com>
2023-12-10 01:27:15 -0300

    H2

--- src/main/java/com/github/youssfbr/cursomc/entities/Category.java
+++ src/main/java/com/github/youssfbr/cursomc/entities/Category.java
@@ -1,14 +1,21 @@
 package com.github.youssfbr.cursomc.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "tb_category")
 @EqualsAndHashCode(of = "id")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 30, nullable = false)
     private String name;
 }
